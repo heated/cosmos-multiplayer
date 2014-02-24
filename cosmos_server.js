5f4cf915de59ff322a7708d469726aecf6abc5f3
rorysaur <ou.rory@gmail.com>
2014-02-24 14:30:20 -0800

    fix port for heroku

--- cosmos_server.js
+++ cosmos_server.js
@@ -16,6 +16,7 @@ var server = http.createServer(function (request, response) {
     }).resume();
 });
 
+var port = Number(process.env.PORT || 5000);
 gameServer.start(server);
-server.listen(8080);
-console.log("Server running at http://127.0.0.1:8080/...");
+server.listen(port);
+// console.log("Server running at http://127.0.0.1:8080/...");
